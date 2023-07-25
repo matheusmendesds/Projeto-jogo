3323a208d48e4019419efa27cd2cc283891e8c09
Matheusms30 <mendesmatheus43@gmail.com>
2023-07-25 20:36:18 -0300

    att

--- jogo.js
+++ jogo.js
@@ -14,6 +14,7 @@ let int = document.getElementById("intPer")
 
 
 function jogar() {
+    document.getElementById("Persona").style.display="none"
     let a = document.getElementById("Arena")
     let pontMon = 0
     let pontPer = 0
@@ -32,7 +33,7 @@ function jogar() {
             a.innerHTML += `<p>Voce é mais lento que o monstro :( </p>`
             pontMon = pontMon + 1
         } 
-            if (mUm.inte < v.value) {
+            if (mUm.inte < int.value) {
                 a.innerHTML += `<p> Você é mais inteligente que o Monstro!</p>`
                 pontPer = pontPer + 1
             } else {
@@ -53,6 +54,7 @@ function jogar() {
 }
 
 function limpar() {
+    document.getElementById("Persona").style.display="block"
     let a = document.getElementById("Arena")
     pontMon = 0
     pontPer = 0
@@ -60,7 +62,51 @@ function limpar() {
     
 }
 
+var mDs = new Object () ;
+mDs.nome = "Mago" ;
+mDs.forca = 55 ;
+mDs.velo = 68 ;
+mDs.inte = 74 ;
+
 function novaFase() {
-    document.getElementById("segFase").style.display="flex"
+    document.getElementById("tntn").style.display="none"
+    document.getElementById("jogar").style.display="none"
+    let s = document.getElementById("segFase")
+    s.style.display="block"
+    let pontMon = 0
+    let pontPer = 0
+    s.innerHTML = `<h2>${nome.value} x ${mDs.nome} </h2>`
+    if (mDs.forca < f.value) {
+        s.innerHTML += `<p> Voce é mais forte que o Monstro!</p>`
+        pontPer = pontPer + 1
+    } else {
+        s.innerHTML += `<p>Voce é mais fraco que o monstro :( </p>`
+        pontMon = pontMon + 1
+    } 
+        if (mDs.velo < v.value) {
+            s.innerHTML += `<p> Voce é mais veloz que o Monstro!</p>`
+            pontPer = pontPer + 1
+        } else {
+            s.innerHTML += `<p>Voce é mais lento que o monstro :( </p>`
+            pontMon = pontMon + 1
+        } 
+            if (mDs.inte < int.value) {
+                s.innerHTML += `<p> Você é mais inteligente que o Monstro!</p>`
+                pontPer = pontPer + 1
+            } else {
+                s.innerHTML += `<p> O monstro é mais inteligente que você :( </p>`
+                pontMon = pontMon + 1
+            } 
+            do {
+                if (pontPer > pontMon) {
+                    qtdM == qtdM - 1
+                    s.innerHTML += "Você venceu a luta!"
+                    document.getElementById("segfase").style.display="flex"
+                    
+                } else {
+                    a.innerHTML += "Você perdeu a luta, tente novamente!"      
+                }
+            } while (qtdM == 0)
+            
 }
 
